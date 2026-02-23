9a77d1e38ec8bd870da63a15a416de651b907ee9
ddmhyang <ddmhyang@gmail.com>
2026-02-24 07:26:05 +0900

    Delete School directory

--- School/fix.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'common.php';
-try {
-    sql_exec("ALTER TABLE School_Members ADD COLUMN injury INT DEFAULT 0");
-    echo "injury 컬럼 추가 완료";
-} catch(Exception $e) {
-    echo "이미 있거나 오류: " . $e->getMessage();
-}
-?>
